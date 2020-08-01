9e83f0e86593bbe77b0182a24144dbe68f69d8f1
Alejandro Ramirez <alexkey0516@gmail.com>
2020-08-01 10:30:11 -0500

    docs agregado

---
+++ docs/precache-manifest.73e8a045b1cfba71c4d1d59dc8f20543.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7fe7cc8777054f9644c88bf144e554",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f2cd0182421f4ec3035",
+    "url": "/static/css/main.d77c4885.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f2cd0182421f4ec3035",
+    "url": "/static/js/main.7acf2de9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
